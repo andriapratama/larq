794ea63db4d8f3ab495608c875bd8aae9726bfd6
andriapratama <andrilocong1st@gmail.com>
2024-08-02 16:57:06 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,233 @@
-import Image from "next/image";
+'use client';
+
+import { AlignJustify, ChevronLeft, ChevronRight } from 'lucide-react';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const data: {
+  firstName: string;
+  secondName: string;
+  image: string;
+  firstColor: string;
+  secondColor: string;
+}[] = [
+  {
+    firstName: 'Himalayan',
+    secondName: 'Pink',
+    image: '/images/pink.webp',
+    firstColor: '#FFE3E0',
+    secondColor: '#FCBBB9',
+  },
+  {
+    firstName: 'Granite',
+    secondName: 'White',
+    image: '/images/white.webp',
+    firstColor: '#ffffff',
+    secondColor: '#D6DBD4',
+  },
+  {
+    firstName: 'Monaco',
+    secondName: 'Blue',
+    image: '/images/blue.webp',
+    firstColor: '#607C8E',
+    secondColor: '#13264D',
+  },
+  {
+    firstName: 'Stone',
+    secondName: 'Black',
+    image: '/images/black.webp',
+    firstColor: '#3C4142',
+    secondColor: '#141A1F',
+  },
+];
 
 export default function Home() {
+  const [indexActive, setIndexActive] = useState<number>(0);
+  const [isDetail, setIsDetail] = useState<boolean>(false);
+
+  const handleIncrease = () => {
+    if (indexActive < data.length - 1) {
+      setIndexActive((prev) => prev + 1);
+    }
+
+    if (indexActive === data.length - 1) {
+      setIndexActive(0);
+    }
+  };
+
+  const handleDecrease = () => {
+    if (indexActive > 0) {
+      setIndexActive((prev) => prev - 1);
+    }
+
+    if (indexActive === 0) {
+      setIndexActive(data.length - 1);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main
+      className="w-full min-h-screen relative overflow-hidden duration-500"
+      style={{ backgroundColor: data[indexActive].firstColor }}
+    >
+      <nav className="w-full flex justify-between fixed z-50 top-0 h-[100px] items-center px-10">
+        <p
+          className="text-2xl font-gruppo font-semibold duration-300"
+          style={{ color: data[indexActive].secondColor }}
+        >
+          LARQ
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <AlignJustify
+          className="duration-300"
+          style={{ color: data[indexActive].firstColor }}
+        />
+      </nav>
+
+      <div
+        className={`min-h-screen flex  absolute items-center right-0 ease-linear transition-all ${
+          isDetail
+            ? 'aspect-square rounded-l-[600px] z-10 justify-start translate-x-1/2 duration-700'
+            : 'w-1/2 duration-500 justify-center'
+        }`}
+        style={{ backgroundColor: data[indexActive].secondColor }}
+      ></div>
+
+      <div
+        className={`w-full flex absolute inset-0 duration-500 delay-200 z-20 ${
+          isDetail ? 'opacity-0' : 'opacity-100'
+        }`}
+      >
+        <div className="w-1/2 min-h-screen flex justify-center items-center">
+          {data.map((item, index) => (
+            <p
+              className={`text-[100px] font-bold font-inter absolute ease-in-out transition-all ${
+                index === indexActive
+                  ? 'opacity-100 delay-200 duration-300'
+                  : 'opacity-0 translate-x-[200px] duration-300'
+              }`}
+              key={index}
+              style={{ color: item.secondColor }}
+            >
+              {item.firstName}
+            </p>
+          ))}
+        </div>
+        <div className="w-1/2 min-h-screen flex justify-center items-center transition-all">
+          {data.map((item, index) => (
+            <p
+              className={`text-[100px] font-bold font-inter absolute ease-in-out transition-all ${
+                index === indexActive
+                  ? 'opacity-100 delay-200 duration-300'
+                  : 'opacity-0 -translate-x-[200px] duration-300'
+              }`}
+              key={index}
+              style={{ color: item.firstColor }}
+            >
+              {item.secondName}
+            </p>
+          ))}
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="w-full h-screen absolute z-20 flex justify-center items-center">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src={data[indexActive].image}
+          alt={data[indexActive].secondName}
+          className={`w-[1200px] h-auto object-cover object-center aspect-auto ease-linear transition-all -translate-y-[5%] ${
+            isDetail ? '-translate-x-[60%] duration-700' : 'duration-500'
+          }`}
           priority
+          width={1200}
+          height={1200}
+          quality={100}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="absolute bottom-8 left-10 flex gap-3 z-30">
+        <button
+          type="button"
+          className={`w-10 h-10 rounded-full flex justify-center items-center absolute duration-300 ease-linear transition-all ${
+            isDetail ? 'opacity-100 z-30' : 'opacity-0 -z-10'
+          }`}
+          onClick={() => setIsDetail(false)}
+          style={{ backgroundColor: data[indexActive].secondColor }}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <ChevronLeft style={{ color: data[indexActive].firstColor }} />
+        </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          type="button"
+          className={`w-10 h-10 rounded-full flex justify-center items-center relative duration-300 ease-linear transition-all ${
+            isDetail ? 'opacity-0 -z-10' : 'opacity-100 z-30'
+          }`}
+          onClick={() => handleDecrease()}
+          style={{ backgroundColor: data[indexActive].secondColor }}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <ChevronLeft
+            className="duration-300"
+            style={{ color: data[indexActive].firstColor }}
+          />
+        </button>
+        <button
+          type="button"
+          className={`w-10 h-10 rounded-full flex justify-center items-center relative duration-300 ease-linear transition-all ${
+            isDetail ? 'opacity-0 -z-10' : 'opacity-100 z-30'
+          }`}
+          onClick={() => handleIncrease()}
+          style={{ backgroundColor: data[indexActive].secondColor }}
+        >
+          <ChevronRight
+            className="duration-300"
+            style={{ color: data[indexActive].firstColor }}
+          />
+        </button>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        className={`absolute bottom-8 right-10  ${
+          isDetail ? 'opacity-0 -z-10' : 'opacity-100 z-30'
+        }`}
+      >
+        <button
+          className="w-[120px] h-[40px] rounded-full font-semibold flex justify-center items-center shadow-custom border-b border-r border-solid border-white/30 duration-500 ease-linear transition-all"
+          style={{
+            backgroundColor: data[indexActive].secondColor,
+            color: data[indexActive].firstColor,
+          }}
+          onClick={() => setIsDetail(true)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          Buy
+        </button>
+      </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        className={`absolute right-[20px] top-1/2 -translate-y-1/2 -translate-x-1/2 z-30 duration-500 ease-linear transition-all ${
+          isDetail ? 'opacity-100 delay-300' : 'opacity-0'
+        }`}
+      >
+        <button
+          className="w-[200px] h-[65px] text-2xl rounded-full font-semibold  flex justify-center items-center shadow-custom border-b border-r border-solid border-white/30 duration-500 ease-linear transition-all"
+          style={{
+            backgroundColor: data[indexActive].secondColor,
+            color: data[indexActive].firstColor,
+          }}
+          onClick={() => setIsDetail(true)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Buy
+        </button>
+      </div>
+
+      <div
+        className={`w-[900px] absolute left-[47%] -translate-x-1/2 top-1/2 -translate-y-1/2 z-20 duration-500 ease-linear transition-all ${
+          isDetail ? 'opacity-100 delay-700' : 'opacity-0'
+        }`}
+      >
+        <h1 className="w-full text-center text-gray-500 text-3xl font-semibold">
+          LARQ Bottle PureVis
+        </h1>
+        <h2 className="text-[90px] text-gray-500 font-bold">
+          {data[indexActive].firstName} {data[indexActive].secondName}
+        </h2>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: 'var(--font-inter)',
+        gruppo: 'var(--font-gruppo)',
+      },
+      boxShadow: {
+        custom:
+          '10px 10px 20px 0px rgba(0, 0, 0, 0.3), -5px -5px 15px 0px rgba(225, 225, 225, 0.2), inset 0 3px 4px 0 rgba(0, 0, 0, 0.05)',
       },
     },
   },
